77df469473b8ad75a56dab2d4f363a61996c22bd
Rakesh Samaddar <rasamad+github@gmail.com>
2020-04-15 04:12:47 +0530

    basic map in place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { APIService } from './services/api.service';
 import { CityModel } from './models/cities.model';
+import {} from 'googlemaps';
 import * as _ from 'lodash';
 
 @Component({
@@ -9,40 +10,53 @@ import * as _ from 'lodash';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-//   title = 'india-cities-angular';
-private cityPromise: Promise<CityModel[]>;
-// cities: CityModel[];
-stateCityMapping: {
-    [key: string]: CityModel[];
-};
-states: string[];
-stateSelected: string;
+    private cityPromise: Promise<CityModel[]>;
+    stateCityMapping: {
+        [key: string]: CityModel[];
+    };
+    states: string[];
+    stateSelected: string;
+    @ViewChild('map') mapElement: any;
+    map: google.maps.Map;
 
-constructor(
-    public apiService: APIService
-) {
-    this.stateCityMapping = {};
-    // this.stateSelected = "Select a state";
-}
-
-ngOnInit() {
-    this.cityPromise = this.apiService.getCities().toPromise();
-    this.initialSetup();
-}
-
-async initialSetup() {
-    let cities = await this.cityPromise;
-    // console.log(this.cities);
-    let unionTerritory = /\*$/;
-    for(let city of cities) {
-        if (unionTerritory.test(city.State)) {
-            city.State = city.State.slice(0,city.State.length-2);
-        }
-        this.stateCityMapping[city.State] = this.stateCityMapping[city.State] || [];
-        this.stateCityMapping[city.State].push(city);
+    constructor(
+        public apiService: APIService
+    ) {
+        this.stateCityMapping = {};
     }
-    this.states = Object.keys(this.stateCityMapping).sort();
-    cities.length = 0;
-}
+
+    ngOnInit():void {
+        this.cityPromise = this.apiService.getCities().toPromise();
+        this.initialSetup();
+    }
+
+    async initialSetup() {
+        let cities = await this.cityPromise;
+        // console.log(this.cities);
+        let unionTerritory = /\*$/;
+        for(let city of cities) {
+            if (unionTerritory.test(city.State)) {
+                city.State = city.State.slice(0,city.State.length-2);
+            }
+            this.stateCityMapping[city.State] = this.stateCityMapping[city.State] || [];
+            this.stateCityMapping[city.State].push(city);
+        }
+        this.states = Object.keys(this.stateCityMapping).sort();
+        cities.length = 0;
+    }
+
+    public ngAfterViewInit(): void {
+        // Location of India center
+        const indiaCent = {lat: 22.0586763, lng: 78.9359167};
+        let mapProperties: google.maps.MapOptions = {
+            center: indiaCent,
+            zoom: 5,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            mapTypeControl: false,
+            streetViewControl: false,
+        }
+        debugger;
+        this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
+    }
 
 }
